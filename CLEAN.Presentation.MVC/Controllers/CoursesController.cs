e3e6289b192e442167b7e0283aeed8288592bd10
rBaun <runeb95@gmail.com>
2021-06-16 20:12:46 +0200

    Added the Courses page, including controller, dummy data, styling, view and authentication

---
+++ CLEAN.Presentation.MVC/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CLEAN.Application.Contracts;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CLEAN.Presentation.MVC.Controllers
+{
+    [Authorize]
+    public class CoursesController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
