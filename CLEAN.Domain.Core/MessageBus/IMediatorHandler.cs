656541aeccb3738a45be42de51c51cc7384287dc
rBaun <runeb95@gmail.com>
2021-06-16 23:20:15 +0200

    Added support for commands and events with MediatR

---
+++ CLEAN.Domain.Core/MessageBus/IMediatorHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CLEAN.Domain.Core.Commands;
+
+namespace CLEAN.Domain.Core.MessageBus
+{
+    public interface IMediatorHandler
+    {
+        Task SendCommand<T>(T command) where T : Command;
+    }
+}
--- CLEAN.Infrastructure.MessageBus/InMemoryBus.cs
+++ CLEAN.Infrastructure.MessageBus/InMemoryBus.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using CLEAN.Domain.Core.Commands;
+using CLEAN.Domain.Core.MessageBus;
+using MediatR;
 
 namespace CLEAN.Infrastructure.MessageBus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
